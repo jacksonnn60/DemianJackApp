ac8125e46d7ee80059a5564e3e0d397e1e76a56b
Yevhen Basistyi <psik726@gmail.com>
2023-06-03 16:31:16 +0200

    first commit

---
+++ DemianJackApp/DemianJackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemianJackAppApp.swift
+//  DemianJackApp
+//
+//  Created by Jackson  on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemianJackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
